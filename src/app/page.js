f012e955e7a11764c50320e41c69c23750a2ba3a
ankitkumar5962 <159166776+ankitkumar5962@users.noreply.github.com>
2024-05-15 11:22:09 -0700

    some changes

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-red-500 text-green-500 text-center">
+     hello how are you sir
+    </main>
+  );
+}
