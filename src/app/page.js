1a2440e4c7e8333dfc7ddbd2c4176912f4137edb
ankitkumar5962 <159166776+ankitkumar5962@users.noreply.github.com>
2024-05-15 11:36:37 -0700

    new code added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,32 @@
-import Image from "next/image";
+
+
+import Swiper from "./components/Swiper";
+import IntroContainer from "./components/introContainer";
+import IntroBigBanner from "./components/introBigBanner";
+import TestiMonials from "./components/testimonials";
+
+// import CardWithImage from "./components/cardwithimage";
+import DownToUp from "./components/downtoup";
+// import TextContentSection from "./components/textContentSection";
+import HeroWithImage from "./components/heroWithImage";
+import FourBoxSection from "./components/fourBoxSection";
+
+
 
 export default function Home() {
   return (
-    <main className="bg-red-500 text-green-500 text-center">
-     hello how are you sir
+    <main>
+  <Swiper/>
+  <IntroContainer/>
+  <IntroBigBanner/>
+  
+  {/* <CardWithImage/> */}
+  
+  {/* <TextContentSection/> */}
+  <FourBoxSection/>
+  <DownToUp/>
+  <HeroWithImage/>
+  <TestiMonials/>
     </main>
   );
 }
