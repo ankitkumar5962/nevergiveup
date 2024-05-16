3b2c72ff61e84a64710881da1231ff4faba9a9dc
ankitkumar5962 <159166776+ankitkumar5962@users.noreply.github.com>
2024-05-16 13:06:20 -0700

    Update Swiper.js

--- src/app/components/Swiper.js
+++ src/app/components/Swiper.js
@@ -11,7 +11,7 @@ import firstslide1 from "./../../../public/frontslide1.jpg";
 import firstslide2 from "./../../../public/frontslide2.png";
 // import firstslide3 from "./../../../public/frontslide3.png";
 import firstslide4 from "./../../../public/frontslide4.png";
-import firstslide5 from '../../../public/frontslide5.png';
+//import firstslide5 from '../../../public/frontslide5.png';
 import firstslide6 from "./../../../public/frontslide6.png";
 // import firstslide7 from "./../../../public/frontslide7.png";
 import firstslide8 from "./../../../public/frontslide8.png";
@@ -58,7 +58,7 @@ export default function App() {
           <Image src={firstslide4} fill={true} alt="slideimg3"/>
         </SwiperSlide>
         <SwiperSlide className="h-[500px] w-screen max-md:h-[240px] ">
-          <Image src={firstslide5} fill={true} alt="slideimg5"/>
+          <Image src={""} fill={true} alt="slideimg5"/>
         </SwiperSlide>
         <SwiperSlide className="h-[500px] w-screen max-md:h-[240px] ">
           <Image src={firstslide6} fill={true} alt="slideimg6"/>
